3172a84a6730a56523a0cdec4aba31ac9a28d69b
israa503 <israa503@gmail.com>
2026-05-21 16:00:49 +0100

    fixed firestore export

--- diagnostic-platform/src/firebase/firebase.js
+++ diagnostic-platform/src/firebase/firebase.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLwfDqahYiEdAlOESJF8Vjq0NTNS3M2nk",
-  authDomain: "diagnostic-platform-f524a.firebaseapp.com",
-  projectId: "diagnostic-platform-f524a",
-  storageBucket: "diagnostic-platform-f524a.firebasestorage.app",
-  messagingSenderId: "392734465750",
-  appId: "1:392734465750:web:8fc15a4498fca5cb76f377"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
